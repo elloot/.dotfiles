aa7539e2a21fd0b04711e9afd4e3ef95b61b9a43
elloot <54401777+elloot@users.noreply.github.com>
2021-09-23 12:58:06 +0200

    ✨ remove plugins dotfiles, add installation of said plugins to script

--- ubuntu/_symlink.sh
+++ ubuntu/_symlink.sh
@@ -43,3 +43,9 @@ done
 
 # Install powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+# Install syntax highlighting plugin
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# Install auto suggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
